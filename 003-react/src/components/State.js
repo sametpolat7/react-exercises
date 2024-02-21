899205553c51978afb46bf0a1db6b9598aafb091
Samet Polat <samet.polat59@hotmail.com>
2024-02-21 19:31:53 +0300

    components/State.js added

--- 003-react/src/components/State.js
+++ 003-react/src/components/State.js
@@ -1,46 +1,105 @@
 import React, { useState } from 'react';
 
 function State() {
-    const [name, setName] = useState("Guest");
+    const [name, setName] = useState("Guest!");
 
-    const [friends, setFriends] = useState(["Ali", "Kel"]);
+    const [friends, setFriends] = useState([]);
+    const myFriends = ["Ali", "Kel", "Özlem", "Onur"]
+    const [index, setIndex] = useState(0);
     const [address, setAddress] = useState({title: "Merkez/Çanakkale", zip: 17100});
-
     const [count, setCount] = useState(0);
-
     const increment = () => {
         return setCount(count + 1);
     }
-
     const decrement = () => {
         if(count > 0) {
             setCount(count - 1);
         }
         return;
     }
+    const [inpName, setInpName] = useState("");
+    const [inpSurname, setInpSurname] = useState("");
+    const [form, setForm] = useState({name: "", surname: ""});
+    const changeForm = (event) => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
+
     return (
         <>
-        <h1>Hoşgeldiniz {name}</h1>
+        <h1>Hello {name}</h1>
         <button onClick={() => {setName("Samet!")}} >Log In</button>
+
 
         <h1>My Friends</h1>
         <ul>
         {
-            friends.map((friend, index) => {
-                return <li key={index}>{friend}</li>;
+            friends.map((friend, key) => {
+                return <li key={key}>{friend}</li>;
             })
         }
         </ul>
-        <button onClick={() => {setFriends([...friends, "Onur"])}}>Add Friend</button>
-        
+        <button onClick={() => {
+            setIndex(index < myFriends.length - 1 ? index + 1 : index);
+            setFriends(friends.includes("Onur") ? [...friends] : [...friends, myFriends[index]]);
+        }}>Add New Friend!
+        </button>
+
+        <button onClick={() => {
+            setIndex(index > 0 ? index - 1 : index);
+            friends.splice(index, 1);
+            setFriends([...friends]);
+        }}>Discard a Friend!
+        </button>
+
+
         <h1>My Address</h1>
-        <address>{address.title} -{address.zip}</address>
-        <br></br>
-        <button onClick={() => {setAddress({title: "Gökçeada/Çanakkale", zip: 17777})}}>Change address</button>
-        
+        <h2>
+            {
+                Object.values(address).map((value, key) => {
+                    return <span key={key}>{value} </span>
+                })
+            }
+        </h2>
+        <button onClick={() => {
+            setAddress({...address, street: "Esenler Mh", v: "Cetin Emec Cad"})
+            }}>Display full address</button>
+
+
         <h1>Counter : {count}</h1>
         <button onClick={decrement}>Decrease</button>
         <button onClick={increment}>Increase</button>
+        <br/><br/>
+
+
+        <div>
+            <h1>Form Management with State</h1>
+            <label>Name: </label>
+            <input type='text' value={inpName} onChange={(event) => {
+                return setInpName(event.target.value)
+            }}></input>
+            &nbsp;&nbsp;
+            <label>Surname: </label>
+            <input type='text' value={inpSurname} onChange={(event) => {
+                return setInpSurname(event.target.value);
+            }}></input>
+            <br/><br/>
+            {`Name : ${inpName}`} 
+            <br/>
+            {`Surname : ${inpSurname}`}
+        </div>
+
+        <div>
+            <h1>Easy One! Also Form Management</h1>
+            <label>Name: </label>
+            <input type='text' name='name' value={form.name} onChange={changeForm}></input>
+            &nbsp;&nbsp;
+            <label>Surname: </label>
+            <input type='text' name='surname' value={form.surname} onChange={changeForm}></input>
+            <br/><br/>
+            {`Name : ${form.name}`} 
+            <br/>
+            {`Surname : ${form.surname}`}
+        </div>
         </>
     )
 }
--- 004-react-info/state.js
+++ 004-react-info/state.js
@@ -1,11 +1,29 @@
+// ==== State ====
+// React'te State, bir componentin mevcut durumunu veya verilerini temsil eden bir nesneyi ifade eder. Esasen bir componentin dahili olarak koruyabileceği ve yönetebileceği bilgilerdir.
 
+// 1. Veri Depolama: State, React componentlerinin zaman içinde değişebilen verileri depolamasına ve yönetmesine olanak tanır. Bu veriler, kullanıcı girdisini, sunucu yanıtlarını veya bileşenle ilgili diğer dinamik bilgileri içerebilir.
 
+// 2. Değiştirilebilir: Değişmez olan ve üst componentten alt componente aktarılan prop'ların aksine state değişkendir ve component içinde güncellenebilir.
+
+// 3. Component'e Özel: React'teki her component kendi state'ine sahip olabilir. Bu, bir component'in state'inin, açıkça props olarak aktarılmadığı sürece başka bir component'in state'ini etkilemediği anlamına gelir.
+
+// 4. Component için Yerel: State, tanımlandığı component için yereldir. Diğer componentler tarafından doğrudan erişilemez veya değiştirilemez. Ancak, bir component state'ini alt componentlere prop olarak aktarabilir.
+
+// 5. State Başlatma: State genellikle bir class component'inin kurucusunda this.state = { /* initial state */ } kullanılarak başlatılır. Fonksiyonel bileşenlerde state, React 16.8'de tanıtılan useState kancası kullanılarak yönetilebilir.
+
+// 6. State Güncelleme: Bir component'in state'ini güncellemek için, this.state dosyasını asla doğrudan değiştirmemelisiniz. Bunun yerine, React tarafından sağlanan setState() yöntemini kullanmalısınız. Bu, React'in state değişikliklerini düzgün bir şekilde izleyebilmesini ve uzlaştırabilmesini sağlar.
+
+// 7. Asenkron Güncellemeler: setState() asenkron olabilir, bu da React'in performans nedenleriyle birden fazla state güncellemesini toplu olarak yapabileceği anlamına gelir. Bu nedenle, state'i önceki state'e göre güncellerken, uygun senkronizasyonu sağlamak için setState()'in fonksiyon formunu kullanmanız önerilir.
+
+// 8. Durumsuz Bileşenler: React'teki durumsuz veya işlevsel bileşenler varsayılan olarak duruma sahip değildir. Bununla birlikte, React'te kancaların kullanılmaya başlanmasıyla, işlevsel bileşenler artık useState gibi kancaları kullanarak state'e de sahip olabilir.
+
+// Genel olarak state, React'te bileşenlerin veri veya kullanıcı etkileşimlerindeki değişiklikleri yönetmesini ve bunlara yanıt vermesini sağlayarak React uygulamalarını dinamik ve etkileşimli hale getiren temel bir kavramdır.
 
 // React state (durum) değişikliklerini yönetir ve kullanıcı arayüzünü "uzlaşma" adı verilen bir süreç aracılığıyla günceller. İşte nasıl çalıştığı:
 
 // 1. State Değişiklikleri: Bir React componentin state'i değiştiğinde (genellikle kullanıcı etkileşimleri veya diğer olaylar nedeniyle), React bu değişikliği algılar.
 
-// 2. Sanal DOM Güncellemesi: React daha sonra bileşenin yeni durumunu yansıtmak için sanal DOM'u günceller.
+// 2. Sanal DOM Güncellemesi: React daha sonra componentnin yeni Statenu yansıtmak için sanal DOM'u günceller.
 
 // 3. Fark Algoritması (Diffing): React, güncellenmiş sanal DOM'u sanal DOM'un önceki sürümüyle (anlık durum görüntüsü) karşılaştırmak için hızlı ve verimli bir fark algoritması kullanır.
 
@@ -13,6 +31,6 @@
 
 // 5. Reconciliation (Uzlaşma): React, bu minimum değişiklikleri gerçek DOM'a uygulayarak yalnızca gerekli güncellemelerin yapılmasını sağlar. Bu süreç uzlaştırma olarak bilinir.
 
-// 6. Bileşen Yeniden Oluşturma: React, güncellenmiş durumu yansıtmak için etkilenen bileşeni ve alt bileşenlerini gerçek DOM'da yeniden işler.
+// 6. componentn Yeniden Oluşturma: React, güncellenmiş State yansıtmak için etkilenen componentni ve alt componentnlerini gerçek DOM'da yeniden işler.
 
-// Bu süreci kullanarak React, kullanıcı arayüzü güncellemelerinin verimli bir şekilde gerçekleştirilmesini sağlayarak gereken DOM manipülasyon miktarını en aza indirir ve uygulamanın performansını artırır. Bu yaklaşım ayrıca DOM'u doğrudan manipüle etmenin karmaşıklıklarını soyutlayarak geliştiricilerin uygulama mantığını ve UI bileşenlerini oluşturmaya odaklanmasına olanak tanır.
+// Bu süreci kullanarak React, kullanıcı arayüzü güncellemelerinin verimli bir şekilde gerçekleştirilmesini sağlayarak gereken DOM manipülasyon miktarını en aza indirir ve uygulamanın performansını artırır. Bu yaklaşım ayrıca DOM'u doğrudan manipüle etmenin karmaşıklıklarını soyutlayarak geliştiricilerin uygulama mantığını ve UI componentlerini oluşturmaya odaklanmasına olanak tanır.
